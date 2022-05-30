3f3c9aec78dd1cf31279fd6d111049b0feb89828
Jock Dalby <jock.dalby@citrusad.com>
2022-05-30 14:19:39 +1000

    add Card and Avatar components and styling

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 import './UserItem.css';
 
 const UserItem = props => {
   const { user } = props
   return (
     <li className="user-item">
-      <div className="user-item__content">
+      <Card className="user-item__content">
+        <Link to={`/${user.id}/places`}>
         <div className="user-item__image">
-          <img src={user.image} alt={user.name}/>
+          <Avatar image={user.image} alt={user.name}/>
         </div>
         <div className="user-item__info">
           <h2>{user.name}</h2>
           <h3>{user.places} {user.places === 1 ? 'Place' : 'Places'}</h3>
         </div>
-      </div>
+        </Link>
+      </Card>
     </li>
   )
 };
