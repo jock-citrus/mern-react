5327c9a2005155c583a31c55c491400efc5cef4b
Jock Dalby <jock.dalby@citrusad.com>
2022-05-30 15:50:06 +1000

    create Navigation components and add to App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-d
 
 import Users from './users/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact><Users /></Route>
-        <Route path="/places/new" exact><NewPlace /></Route>
-        <Redirect to="/"/>
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact><Users /></Route>
+          <Route path="/places/new" exact><NewPlace /></Route>
+          <Redirect to="/"/>
+        </Switch>
+      </main>
     </Router>
   );
 }
--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import UserItem from './UserItem';
-
+import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
 const UsersList = props => {
   if (!props.users.length) {
-    return <div class="center"><h2>No users found.</h2></div>
+    return <div class="center"><Card><h2>No users found.</h2></Card></div>
   }
 
   return <ul className="users-list">
