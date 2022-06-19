c54a0fc772d4fb883478c1d9789a162150ee1a8b
Jock Dalby <jock.dalby@citrusad.com>
2022-06-19 13:36:02 +1000

    listen to auth context changes in NavLinks

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = () => {
+  const auth = useContext(AuthContext)
   // NavLink is same as Link but interprets current route and adds active
   // pseudo class to nested anchor tag etc.
   return <ul className="nav-links">
     <li>
       <NavLink to="/" exact>ALL USERS</NavLink>
     </li>
-    <li>
+    {auth.isLoggedIn && <li>
       <NavLink to="/u1/places">MY PLACES</NavLink>
-    </li>
-    <li>
+    </li>}
+    {auth.isLoggedIn && <li>
       <NavLink to="/places/new">ADD PLACE</NavLink>
-    </li>
-    <li>
+    </li>}
+    {!auth.isLoggedIn && <li>
       <NavLink to="/auth">AUTH</NavLink>
-    </li>
+    </li>}
   </ul>
 }
 
